ee13a39bbde04fa0dc74a0db40410877c9aa36d0
magusd <vitor.lobs@gmail.com>
2021-03-01 23:07:23 -0300

    so far so good

---
+++ src/pages/about.js
@@ -0,0 +1,36 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function about() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, culpa
+          quo repudiandae hic, doloremque amet expedita illo dolor nisi
+          necessitatibus pariatur commodi quae enim odit non maiores alias minus
+          at.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, culpa
+          quo repudiandae hic, doloremque amet expedita illo dolor nisi
+          necessitatibus pariatur commodi quae enim odit non maiores alias minus
+          at.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, culpa
+          quo repudiandae hic, doloremque amet expedita illo dolor nisi
+          necessitatibus pariatur commodi quae enim odit non maiores alias minus
+          at.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, culpa
+          quo repudiandae hic, doloremque amet expedita illo dolor nisi
+          necessitatibus pariatur commodi quae enim odit non maiores alias minus
+          at.
+        </p>
+      </div>
+    </Layout>
+  )
+}

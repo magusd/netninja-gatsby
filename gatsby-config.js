df1b550f4c6cae41a4bd08e07057ac0448222f3b
magusd <vitor.lobs@gmail.com>
2021-03-04 00:33:57 -0300

    static page gen

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,42 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Your Name - Blog`,
+    author: `Your Name`
+  },
+  plugins: [
+    "gatsby-plugin-catch-links", 
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+    // You can have multiple instances of this plugin
+    // to read source nodes from different locations on your
+    // filesystem.
+    //
+    // The following sets up the Jekyll pattern of having a
+    // "pages" directory for Markdown files and a "data" directory
+    // for `.json`, `.yaml`, `.csv`.
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+  ],
 }
---
+++ src/data/index.md
@@ -0,0 +1,13 @@
+---
+path: "/hello-world"
+date: 2017-07-12T17:12:33.962Z
+title: "My First Gatsby Post"
+---
+
+# sd
+
+Oooooh-weeee, my first blog post!
+
+## a
+a
+a
